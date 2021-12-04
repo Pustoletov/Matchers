b8d2fa6d95eb3ae5dea9bae60bf718056ea8ea47
Siun Maksim <myrdym@rambler.ru>
2021-12-04 15:13:31 +0300

    toBe add

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -27,3 +27,17 @@ test('return correctly value 2', () => {
   ];
   expect(heroes).not.toBe(result);
 });
+
+test('toBe', () => {
+  const heroes = sortedHealth([
+    { name: 'bowman', health: 10 },
+    { name: 'swordsman', health: 100 },
+    { name: 'magician', health: 80 },
+  ]);
+  const result = [
+    { name: 'swordsman', health: 100 },
+    { name: 'magician', health: 80 },
+    { name: 'bowman', health: 10 },
+  ];
+  expect(JSON.stringify(heroes)).toBe(JSON.stringify(result));
+});
